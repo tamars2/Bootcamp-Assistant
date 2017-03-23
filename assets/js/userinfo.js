9a5766c6593eb6f56da585dd256242bfdca3613c
Sylvester Goodwin <Sylvester Goodwin>
2017-03-23 09:55:51 -0400

    update userinfo.js and modal code

--- assets/js/userinfo.js
+++ assets/js/userinfo.js
@@ -1,9 +1,10 @@
 
 var dataStore; 
+var bcaCurrentUser;
 
 function connedtToDatabase()
-{
-    // Initialize Firebase
+{ 
+  // Initialize Firebase
   var config = {
     apiKey: "AIzaSyCqfTnrMLjnNr5VPR28AHSDfwbXNhPhMYY",
     authDomain: "bootcampstuff-c6ecb.firebaseapp.com",
@@ -13,16 +14,29 @@ function connedtToDatabase()
   };
   firebase.initializeApp(config);	
 	database = firebase.database();	
-	
+
   return database;
 }
 
-// Set the user name appearing in the navication bar
-function setNavBarUserID(userid)
+function verifyDataEntry()
 {
-  $("#bca-userid").html(userid);
+	data = new userPreferenceEntry();
+	if (data.newUser == "new")
+	{
+		if (data.email != data.confirmEmail )
+		{
+			return false;
+		}
+		if (data.name.length<1)
+		{
+			return false;
+		}
+	}
+	if (data.email.length<4)
+	{
+		return false;
+	}
 }
-
 // Called from the menu bar this opens the login screen
 function editUserPreference()
 {
@@ -31,42 +45,53 @@ function editUserPreference()
   $("#form-dialog").modal("toggle");
 }
 
-// Called from the menu bar when the user clicks on the logout 
-//********* NOT YET IMPLEMENTED
+// strip out unsupported characters
+// replace with placeholders
+function getUserPath(a)
+{
+	return a.split('.').join('-');
+}
+function loadUserFromDB(userid)
+{
 
-function addNewuserToDB(newuser)
+	dataStore.ref('/users/' + userid).once('value').then(function(snapshot) {
+	bcaCurrentUser = snapshot.val();
+	});
+}
+function addNewuserToDB(newUserPref)
 {
   // set path to useremail
-  var  userPath = newuser.email;
+  var  userPath = getUserPath(newUserPref.email);
      
   // save the user data 
-  dataStore.ref('users/' + userPath).set(newuser); 
-  
-  // set info in session memory
-  // user id = email
-  sessionStorage.setItem('GATechCodeAssist', newuser.email);
+  dataStore.ref('users/' + userPath).set(newUserPref); 
 }
 
 // Pulls the data entered unto the user login screen and places into an objcet
 function userPreferenceEntry()
 {	
-	var cbx = $('#bca_cbx');    
-  this.newUser = true; //cbx.context.activeElement.checked;// $("#bca-userpref-cbx-newuser").attr("value");
+  this.newUser = $("#bca-user-login").attr("data-bca-user-type");
   this.name = $("#bca-userpref-name").val().trim();
   this.email = $("#bca-userpref-username").val().trim();
+	this.confirmEmail = $("#bca-userpref-emailid").val().trim();
   this.zipCode = $("#bca-zipcode").val().trim();
 }
 
 // Submits the NewUser infotmation to the database 
-// Need to decide what how this does.
-// NOT YET IMPLEMENTED **************************
 function submitUIPrefdata()
 {
 	var userPref = new userPreferenceEntry();
-  console.log(userPref);
-  addNewuserToDB(userPref);
-  // go to the database and check fo
-  userPref.newUser=false;
+	if (userPref.newUser == "new")
+	{
+		userPref.newUser = "returning";
+		addNewuserToDB(userPref);
+		bcaCurrentUser = userPref;
+	}
+	else
+	{
+	  var userPath = getUserPath(userPref.email);
+		loadUserFromDB(userPath);
+	}
 }
 
 // right now does nothing as there is nothing to do when the worker clicks discard
@@ -75,34 +100,37 @@ function discardUIPrefdata()
 
 }
 
+// Hides and show data entry items on the modal user login screen based on new or returning rb
+function userType(userT)
+{
+	// new
+	if (userT == 1)
+	{
+		$("#bca-user-login").attr("data-bca-user-type","new");
+		$("#bca-userpref-confirmemail-grp").show();   
+		$("#bca-userpref-name-grp").show(); 
+    $("#bca-userpref-username-grp").show(); 
+	  $("#bca-userpref-emailid-grp").show(); 
+    $("#bca-zipcode-grp").show(); 
+		$(".help-text").show()
+	}
+	else // returning
+	{
+		$("#bca-user-login").attr("data-bca-user-type","returning");
+		$("#bca-userpref-confirmemail-grp").hide();  
+		$("#bca-userpref-name-grp").hide(); 
+    $("#bca-zipcode-grp").hide();
+		$(".help-text").hide();
+	}
+}
+
 // Event Handles for the items on the preference page
 function addEventHandlers()
 {
-  $("bca-brand").on("hover", function(e) {
-      displayHoverMessage();
+  $("#bca-userpref-btn_submit").on("hover", function(e) {
+		verifyDataEntry();
   });
-  
-	$("#bca-userpref-cbx-newuser").on("click", function (e) {	
-		var cbx = $('#bca_cbx');    
-    if (cbx.context.activeElement.checked == undefined)
-    {
-      
-    }
-    else
-    {
-      if (cbx.context.activeElement.checked)
-      {
-			$("#bca-userpref-confirmemail-grp").addClass("bca-hide");
-        
-      }
-      else
-      {
-			$("#bca-userpref-confirmemail-grp").removeClass("bca-hide"); 
-        
-      }
-    }
-	});
-	
+  	
 	$("#form-dialog").on("hidden.bs.modal", function (e) {	
   });
 
